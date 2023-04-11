67c83b8b48f6dbcf5be7bb7debefbca1fbed50bc
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-04-11 20:00:56 +0800

    Set up Dockerfile for mariadb (WIP)

--- Makefile
+++ Makefile
@@ -10,9 +10,9 @@ MDB_VOLUME_DIR	= /Users/hyun-zhe/data/mariadb
 WP_VOLUME_DIR	= /Users/hyun-zhe/data/wordpress
 
 COMPOSE_FLAGS	= -f
-UP_FLAGS		= -d --build
-# UP_FLAGS		= --build
-DOWN_FLAGS		= --rmi all
+# UP_FLAGS		= -d --build
+UP_FLAGS		= --build
+DOWN_FLAGS		= --rmi all --remove-orphans
 
 all:	volumes
 	$(COMPOSE) $(COMPOSE_FLAGS) $(SRCS)/$(COMPOSE_FILE) up $(UP_FLAGS)
---
+++ srcs/requirements/mariadb/tools/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+service mysql start
+
+mysql -sfu root << EOF
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER '$MYSQL_ADMIN'@'localhost' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_ADMIN'@'localhost' WITH GRANT OPTION;
+CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
+
+FLUSH PRIVILEGES;
+EOF
