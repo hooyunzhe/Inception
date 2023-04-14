01dd545df62f7ed46dfce705a652a44b2e6322b4
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-04-14 22:29:07 +0800

    Fixed wordpress to work with mariadb container

--- Makefile
+++ Makefile
@@ -10,13 +10,11 @@ MDB_VOLUME_DIR	= /Users/hyun-zhe/data/mariadb
 WP_VOLUME_DIR	= /Users/hyun-zhe/data/wordpress
 
 COMPOSE_FLAGS	= -f
-# UP_FLAGS		= -d --build
-UP_FLAGS		= -d
+UP_FLAGS		= -d --build
 DOWN_FLAGS		= --rmi all --remove-orphans
 
 all:	volumes
-	$(COMPOSE) $(COMPOSE_FLAGS) $(SRCS)/$(COMPOSE_FILE) build
-	$(COMPOSE) $(COMPOSE_FLAGS) $(SRCS)/$(COMPOSE_FILE) up
+	$(COMPOSE) $(COMPOSE_FLAGS) $(SRCS)/$(COMPOSE_FILE) up $(UP_FLAGS)
 
 volumes:
 	$(MKDIR) $(MDB_VOLUME_DIR) $(WP_VOLUME_DIR)
--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -4,14 +4,16 @@ echo "USE mysql;
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE $MYSQL_DATABASE;
 CREATE USER '$MYSQL_ADMIN'@'localhost' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_ADMIN'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_ADMIN'@'localhost' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_ADMIN'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';
 CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_USER_PASSWORD';
 FLUSH PRIVILEGES;" > /tools/init.sql
+
+sed -i s/127.0.0.1/0.0.0.0/ /etc/mysql/mariadb.conf.d/50-server.cnf
 
 mysql_install_db --user=mysql > /dev/null
 mysqld --user=mysql --bootstrap < /tools/init.sql
